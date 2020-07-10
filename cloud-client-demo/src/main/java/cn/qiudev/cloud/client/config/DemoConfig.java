5352a12c89e18b271d560922b913e402e7174705
Shuangqiu Zheng <qiu_zs@126.com>
2020-07-10 11:16:45 +0800

    add monitor admin server & cloud gateway, modify client to specify lb, todo fix spring-cloud-bus & config monitor

--- cloud-client-demo/src/main/java/cn/qiudev/cloud/client/config/DemoConfig.java
+++ cloud-client-demo/src/main/java/cn/qiudev/cloud/client/config/DemoConfig.java
@@ -3,6 +3,7 @@ package cn.qiudev.cloud.client.config;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 /**
--- cloud-client-demo/src/main/java/cn/qiudev/cloud/client/controller/DemoController.java
+++ cloud-client-demo/src/main/java/cn/qiudev/cloud/client/controller/DemoController.java
@@ -2,6 +2,8 @@ package cn.qiudev.cloud.client.controller;
 
 import cn.qiudev.cloud.client.config.DemoConfig;
 import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,14 +13,19 @@ import javax.annotation.Resource;
  * @author qiuqiu
  */
 @RestController
+@Slf4j
 public class DemoController {
+
+    @Value("${eureka.instance.instanceId}")
+    private String instanceId;
 
     @Resource
     private DemoConfig config;
 
 
     @GetMapping("/config")
-    public String config(){
+    public String config() {
+        log.info("{}被调用", instanceId);
         return JSON.toJSONString(config);
     }
 }
