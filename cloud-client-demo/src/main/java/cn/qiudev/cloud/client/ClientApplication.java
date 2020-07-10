b92399d30fa79a417a3a5e9d52bf6d6bb1f4757d
Shuangqiu Zheng <qiu_zs@126.com>
2020-07-10 14:34:46 +0800

    support grafana , prometheus

--- cloud-client-demo/src/main/java/cn/qiudev/cloud/client/ClientApplication.java
+++ cloud-client-demo/src/main/java/cn/qiudev/cloud/client/ClientApplication.java
@@ -1,8 +1,12 @@
 package cn.qiudev.cloud.client;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author qiuqiu
@@ -13,5 +17,11 @@ public class ClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ClientApplication.class, args);
+    }
+
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> configurer(
+            @Value("${spring.application.name}") String applicationName) {
+        return (registry) -> registry.config().commonTags("application", applicationName);
     }
 }
