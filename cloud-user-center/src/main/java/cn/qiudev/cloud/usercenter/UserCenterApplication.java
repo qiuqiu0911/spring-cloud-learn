c63c98c8f73c60b395c6f2c17abc983933981f68
Shuangqiu Zheng <qiu_zs@126.com>
2020-08-05 13:48:23 +0800

    support prometheus discovery and stat

--- cloud-user-center/src/main/java/cn/qiudev/cloud/usercenter/UserCenterApplication.java
+++ cloud-user-center/src/main/java/cn/qiudev/cloud/usercenter/UserCenterApplication.java
@@ -1,7 +1,12 @@
 package cn.qiudev.cloud.usercenter;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 /**
@@ -9,9 +14,16 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
  */
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableDiscoveryClient
 public class UserCenterApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UserCenterApplication.class, args);
+    }
+
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> configurer(
+            @Value("${spring.application.name}") String applicationName) {
+        return (registry) -> registry.config().commonTags("application", applicationName);
     }
 }
